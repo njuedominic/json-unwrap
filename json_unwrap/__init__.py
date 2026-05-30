37cbea0839f371456b447055fc5b051f50b7fda8
njuedominic <njuemugodominic@gmail.com>
2026-05-30 20:57:01 +0300

    Chore: Update import core  functions

--- json_unwrap/__init__.py
+++ json_unwrap/__init__.py
@@ -1,8 +1,8 @@
 # Import main functions 
-from .core import unrwap_data, fetch_json, json_to_csv
+from .core import unwrap_data, fetch_json, json_to_csv
 
 # Define the version of the package
 
 __version__ = '0.1.0'
 
-__all__ = ['unrwap_data', 'fetch_json', 'json_to_csv']
+__all__ = ['unwrap_data', 'fetch_json', 'json_to_csv']
